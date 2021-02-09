43729fa66541aa0ab819f981da208e2318c800f9
Navaneeth-krishnan-nair <71129158+Navaneeth-krishnan-nair@users.noreply.github.com>
2021-02-09 17:00:04 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,34 +1,33 @@
 //WAP to find the distance between two point using 4 functions.
 #include<stdio.h>
 #include<math.h>
-float input_values();
-float calculate_distance(float a1,float b1,float a2,float b2);
-float display(float d);
-float input_values()
+int input()
 {
-    float a1,b1,a2,b2;
-    printf("enter a1 value:");
-    scanf("%f",&a1);
-    printf("enter a2 value:");
-    scanf("%f",&a2);
-    printf("enter b1 value:");
-    scanf("%f",&b1);
-    printf("enter b2 value:");
-    scanf("%f",&b2);
-    calculate_distance(a1,b1,a2,b2);
+    float a;
+    printf("enter points (a1,b1) and points (a2,b2):");
+    scanf("%f",&a);
+    return a;
 }
 float calculate_distance(float a1,float b1,float a2,float b2)
 {
-    float d;
-    d=sqrt((b2-b1)*(b2-b1)+(a2-a1)*(a2-a1));
-    display(d);
+    float dist;
+    dist=sqrt((b2-b1)*(b2-b1)+(a2-a1)*(a2-a1));
+    return dist;
 }
-float display(float d)
+float display(float dist)
 {
-    printf("The distance between two points is:%f",d);
+    printf("The distance between two points is:%f",dist);
+    return dist;
 }
 int main()
 {
-    input_values();
+    float a1,b1,a2,b2,x,dist;
+    a1=input();
+    b1=input();
+    a2=input();
+    b2=input();
+    dist=calculate_distance(a1,b1,a2,b2);
+    display(dist);
     return 0;
 }
+
