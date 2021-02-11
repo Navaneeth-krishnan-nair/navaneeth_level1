00c33446ce089ec3b28eefd399f63ea38e079e84
Navaneeth-krishnan-nair <71129158+Navaneeth-krishnan-nair@users.noreply.github.com>
2021-02-11 09:46:12 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,33 @@
 //WAP to find the sum of two fractions.
+#include<stdio.h>
+typedef struct frac
+{
+    int n,d;
+}frac;
+frac input()
+{
+    frac f;
+    printf("enter the numerator and denominator:");
+    scanf("%d%d",&f.n,&f.d);
+    return f;
+}
+float compute(frac f1,frac f2)
+{
+    float add;
+    add=(((f1.n*f2.d)+(f2.n*f1.d))/(f1.d*f2.d));
+    return add;
+}
+void output(frac f1,frac f2,float add)
+{
+    printf("the addition of (%d/%d) and (%d/%d) is %f",f1.n,f1.d,f2.n,f2.d,add);
+}
+int main()
+{
+    frac f1,f2;
+    float add;
+    f1=input();
+    f2=input();
+    add=compute(f1,f2);
+    output(f1,f2,add);
+    return 0;
+}
