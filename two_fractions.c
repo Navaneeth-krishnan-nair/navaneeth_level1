7b424bca6f372c2c4aabf0169101ab93d3497e3a
Navaneeth-krishnan-nair <71129158+Navaneeth-krishnan-nair@users.noreply.github.com>
2021-03-08 11:23:51 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -11,31 +11,33 @@ frac input()
     scanf("%d%d",&f.n,&f.d);
     return f;
 }
-float compute(frac f1,frac f2)
+float gcd(int x,int y)
 {
-    int gcd;
-    int x=(f1.n*f2.d)+(f2.n*f1.d);
-    int y=(f1.d*f2.d);
-    for (int i=1;i<=x && i<=y;i++)
-    {
-        if(x%i==0 && y%i==0)
-        gcd=i;
-    }
-    return gcd;
+    if(x==0)
+    return y;
+    return gcd(y%x,x);
 }
-void output(frac f1,frac f2,int gcd)
+frac add(frac f1,frac f2)
 {
-    int x=(f1.n*f2.d)+(f2.n*f1.d);
-    int y=(f1.d*f2.d);
-    printf("the addition of (%d/%d) and (%d/%d) is (%d/%d)",f1.n,f1.d,f2.n,f2.d,x/gcd,y/gcd);
+    frac sum;
+    int a=gcd(f1.d,f2.d);
+    sum.n=((f1.n*f2.d)+(f2.n*f1.d))/a,
+    sum.d=(f1.d*f2.d)/a;
+    return sum;
+}
+
+
+void output(frac f1,frac f2,frac sum)
+{
+    printf("the addition of (%d/%d) and (%d/%d) is (%d/%d)",f1.n,f1.d,f2.n,f2.d,sum.n,sum.d);
 }
 int main()
 {
-    frac f1,f2;
+    frac f1,f2,sum;
     int gcd;
     f1=input();
     f2=input();
-    gcd=compute(f1,f2);
-    output(f1,f2,gcd);
+    sum=add(f1,f2);
+    output(f1,f2,sum);
     return 0;
 }
